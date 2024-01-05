468d5b4beccdf049cc2bc63c89beb1765b942a50
vguru23 <113646236+vguru23@users.noreply.github.com>
2024-01-05 00:47:54 -0500

    Add files via upload

--- myapp.js
+++ myapp.js
@@ -1,9 +1,32 @@
 function getComputerChoice() {
     options = ['Rock', 'Paper', 'Scissors']
     number = getRandomIntInclusive(0,2)
-    console.log(options[number])
-    return(options[number])
+    const computerChoice = options[number];
+    const computerSelection = document.querySelector('#computerimage');
+    if (computerChoice == 'Rock') {
+        const computerImage = document.createElement('img');
+        computerImage.src = 'rock.png';
+        computerImage.id = 'computerSelectionImage';
+        computerSelection.appendChild(computerImage);
+    } else if (computerChoice == 'Paper') {
+        const computerImage = document.createElement('img');
+        computerImage.src = 'paper.png';
+        computerImage.id = 'computerSelectionImage';
+        computerSelection.appendChild(computerImage);
+    } else {
+        const computerImage = document.createElement('img');
+        computerImage.src = 'scissor.png';
+        computerImage.id = 'computerSelectionImage';
+        computerSelection.appendChild(computerImage);
+    }
+    return computerChoice;
+}
 
+function clearSelections() {
+    const playerSelection = document.querySelector('#playerimage');
+    const computerSelection = document.querySelector('#computerimage');
+    playerSelection.innerHTML = ''; // Clear playerSelection div
+    computerSelection.innerHTML = ''; // Clear computerSelection div
 }
 
 function getRandomIntInclusive(min, max) {
@@ -23,44 +46,97 @@ player: rock   computer: scissors
 player: paper  computer: rock
 player: scissors computer: paper
 */
-function playRound(playerSelection, computerSelection) {
+
+ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()    
+    computerSelection = computerSelection.toLowerCase()
+    let roundWinner;    
 
     if (playerSelection == computerSelection) {
-        console.log("You tied")
-        return 0
+        roundWinner = 0
     } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        console.log("You Win! " + playerSelection + " beats " + computerSelection)
-        return 1
+        roundWinner = 1
     } else {
-        console.log("You Lose! " + computerSelection + " beats " + playerSelection)
-        return 2
+        roundWinner = 2
     }
 
+    const playerScore = document.querySelector('#playerScore');
+    const computerScore = document.querySelector('#computerScore');
+        if (roundWinner == 1) {
+            let newScore = parseInt(playerScore.textContent) + 1;
+            playerScore.textContent = newScore.toString();
+            
+        } else if (roundWinner == 2) {
+            let newScore = parseInt(computerScore.textContent) + 1;
+            computerScore.textContent = newScore.toString();
+            
+        }
+        if (computerScore.textContent == '5') {
+            alert("Game Over! Computer Won!");
+            gameOver();
+        } else if (playerScore.textContent == '5') {
+            alert("GameOver! You won!")
+            gameOver();
+        }
 }
+
+
+
 
 function game() {
     playerWins = 0
     computerWins = 0
-    for (i = 0; i < 5; i++) {
-        const prompt=require("prompt-sync")({sigint:true});
+    
 
-        let playerSelection = prompt("Type Rock, Paper, or Scissors to play!")
-        roundWinner = playRound(playerSelection,getComputerChoice())
-        if (roundWinner == 1) {
-            playerWins += 1
-        } else if (roundWinner == 2) {
-            computerWins += 1
-        }
-    }
-    if (playerWins > computerWins) {
-        console.log("You Win overall")
-    } else if (computerWins > playerWins) {
-        console.log("You Lose overall!")
-    } else {
-        console.log("You tied overall")
-    }
+        const playerChoices = document.querySelectorAll('.buttonIcon');
+        const playerSelection = document.querySelector('#playerimage');
+        playerChoices.forEach(button => {
+            button.addEventListener('click', function(e) {
+              clearSelections();
+              const targetId = e.currentTarget.id;
+              
+                if (targetId == "rockPlayer") {
+                    const playerImage = document.createElement('img');
+                    playerImage.src = 'rock.png';
+                    playerImage.id = 'playerSelectionImage';
+                    playerSelection.appendChild(playerImage);
+                    const computerChoice = getComputerChoice();
+                    const playerChoice = "rock";
+                    console.log(computerChoice);
+                    console.log(playerChoice);
+                    playRound(playerChoice,computerChoice);
+                } else if (targetId == "paperPlayer") {
+                    const playerImage = document.createElement('img');
+                    playerImage.src = 'paper.png';
+                    playerImage.id = 'playerSelectionImage';
+                    playerSelection.appendChild(playerImage);
+                    const computerChoice = getComputerChoice();
+                    const playerChoice = "paper";
+                    console.log(computerChoice);
+                    console.log(playerChoice);
+                    playRound(playerChoice,computerChoice);
+                } else {
+                    const playerImage = document.createElement('img');
+                    playerImage.src = 'scissor.png';
+                    playerImage.id = 'playerSelectionImage';
+                    playerSelection.appendChild(playerImage) ;
+                    const computerChoice = getComputerChoice();
+                    const playerChoice = "scissors";
+                    console.log(computerChoice);
+                    console.log(playerChoice);
+                    playRound(playerChoice,computerChoice);
+                }
+            })
+        })
+
+        
+    
+    
+}
+
+function gameOver() {
+    window.location.reload();
 }
 
 game()
+
